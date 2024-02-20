cccce5e1370381b9f50cf7a828c4145b0ecc0a0b
KasperTak <118729147+KasperTak@users.noreply.github.com>
2024-02-20 15:50:02 +0100

    Update app_mfp.py

--- app_mfp.py
+++ app_mfp.py
@@ -107,11 +107,11 @@ with tab1:
     # leeftijd controleren voor juiste financieringslastpercentage.
         def vind_woonquote(worksheet, toetsinkomen, debetrente):
         # Zoek de rij die overeenkomt met het toetsinkomen
-            for row in range(1, worksheet.max_row + 1):
+            for row in range(2, worksheet.max_row + 1):
                 if worksheet.cell(row, 1).value == toetsinkomen:
                     # Als toetsinkomen gevonden is, zoek de debetrente in dezelfde rij
                     for col in range(2, worksheet.max_column + 1):
-                        if worksheet.cell(1, col).value == str(debetrente):
+                        if worksheet.cell(1, col).value == debetrente:
                             # Als debetrente gevonden is, return de waarde
                             return worksheet.cell(row, col).value
         
