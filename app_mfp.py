09451a6cf32888950c8026fd9b1329619eead073
KasperTak <118729147+KasperTak@users.noreply.github.com>
2024-02-20 14:48:41 +0100

    Update app_mfp.py

--- app_mfp.py
+++ app_mfp.py
@@ -104,13 +104,22 @@ with tab1:
            # st.write(f"De debetrente is {debetrente}%")
 
     # leeftijd controleren voor juiste financieringslastpercentage.
-  
+        def vind_woonquote(worksheet, toetsinkomen, debetrente):
+        # Zoek de rij die overeenkomt met het toetsinkomen
+        for row in range(1, worksheet.max_row + 1):
+            if worksheet.cell(row, 1).value == toetsinkomen:
+                # Als toetsinkomen gevonden is, zoek de debetrente in dezelfde rij
+                for col in range(2, worksheet.max_column + 1):
+                    if worksheet.cell(1, col).value == debetrente:
+                        # Als debetrente gevonden is, return de waarde
+                        return worksheet.cell(row, col).value
+    
+    # Gebruik de functie om de woonquote te vinden
         if leeftijd < AOW_leeftijd:
-            woonquote = VOOR_AOW.query("toetsinkomen == @toetsinkomen")[debetrente].values[0]
+            woonquote = vind_woonquote(VOOR_AOW, toetsinkomen, debetrente)
             #st.write(f"Het financieringslastpercentage is {woonquote}")
         else:
-            woonquote = NA_AOW.query("toetsinkomen == @toetsinkomen")[debetrente].values[0]
-  
+            woonquote = vind_woonquote(NA_AOW, toetsinkomen, debetrente)
        
             
         finan_ink = gez_ink * woonquote
